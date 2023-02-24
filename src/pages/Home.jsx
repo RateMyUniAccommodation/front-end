51988d1bf800e38f99fcf4536d5815d0bd73cc3e
JustAdamCain <03adamcain@gmail.com>
2023-02-24 16:39:05 +0000

    Added map to view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from '../assets/react.svg'
-import '../Home.css'
+import '../style/Home.css'
 
 function Home() {
   const [count, setCount] = useState(0)
--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,5 +1,21 @@
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import '../style/View.css'
+
 export default function View() {
-    return (
-      <h1>View</h1>
-    );
-  }
+  return (
+  <>
+    <h1>View</h1>
+    <MapContainer className='map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {/* <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker> */}
+    </MapContainer>
+  </>
+  );
+}
