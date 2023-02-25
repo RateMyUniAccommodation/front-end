19f03c82d035d98cfa0be1171fdaded53c394767
Emile Mathieu <emilepc2003@hotmail.com>
2023-02-25 23:01:14 +0000

    Added some pages, fixed css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 import MapView from "./pages/MapView/MapView";
 import Error from "./pages/Error/Error";
 import Navbar from "./components/Navbar/Navbar";
@@ -13,6 +15,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="view" element={<MapView />} />
         <Route path="auth" element={<Auth />} />
         <Route path="*" element={<Error />} />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,44 +3,35 @@ import { Link, useLocation } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import { HomeIcon } from "@heroicons/react/24/solid";
 
-
 const Navbar = () => {
   const location = useLocation();
   const isHomePage = location.pathname === "/home";
-
+  
   return (
     <nav className={styles.navbar}>
       <div className={styles.navbarContainer}>
-        <Link to="/" className={styles.navbarLogo}>
+        <Link to="/home" className={styles.navbarLogo}>
           <HomeIcon className={styles.navbarLogoIcon} />
           <h1 className={styles.navbarLogoTitle}>ratemyuniaccom</h1>
         </Link>
-
         {!isHomePage && (
           <form className={styles.navbarSearchForm}>
             <input type="text" placeholder="Search..." />
             <button type="submit">Go</button>
           </form>
         )}
-
         <ul className={styles.navbarMenu}>
           <li className={styles.navbarMenuItem}>
-            <Link to="/" className={styles.navbarLink}>
-              Home
-            </Link>
-          </li>
-          <li className={styles.navbarMenuItem}>
             <Link to="/about" className={styles.navbarLink}>
-              About
+              About us
             </Link>
           </li>
           <li className={styles.navbarMenuItem}>
             <Link to="/contact" className={styles.navbarLink}>
-              Contact
+              Contact us
             </Link>
           </li>
         </ul>
-
         <div className={styles.navbarRight}>
           <button className={styles.navbarButton}>Sign In</button>
         </div>
