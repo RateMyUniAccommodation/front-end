c2c52f6483d430a189b22c0af689bf299b7d4503
Emile Mathieu <emilepc2003@hotmail.com>
2023-02-25 12:59:29 +0000

    Added each page a folder for more customization

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Auth from "./pages/Auth";
-import Home from "./pages/Home";
-import View from "./pages/View";
-import Error from "./pages/Err";
+import Auth from "./pages/Auth/Auth";
+import Home from "./pages/Home/Home";
+import View from "./pages/Map/View";
+import Error from "./pages/Error/Err";
 
 export default function App() {
   return (
