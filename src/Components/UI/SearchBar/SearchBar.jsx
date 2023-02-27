e3cd6e74fdf6eaa24b3400e47df0fe9ed8153065
Emile Mathieu <emilepc2003@hotmail.com>
2023-02-27 13:35:19 +0000

    added search icon

--- src/Components/UI/SearchBar/SearchBar.jsx
+++ src/Components/UI/SearchBar/SearchBar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
-//import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const SearchBar = () => {
   return (
     <div className={styles.navbarSearchBar}>
+      <MagnifyingGlassIcon className={styles.searchIcon} />
       <input type="text" placeholder="Search..." />
     </div>
   );
