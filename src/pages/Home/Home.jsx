bfb0d9471ac511eebf3dbd55942eeb769e4b9328
Emile Mathieu <emilepc2003@hotmail.com>
2023-03-02 22:49:05 +0000

    Added homescreen SearchBar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import styles from "./Home.module.css";  
+import React from "react";
+import styles from "./Home.module.css";
+import backgroundImage from "../../assets/images/home-background.png";
+import SearchBar from "../../Components/UI/SearchBar/SearchBar";
 
 const Home = () => {
+  //<div className={styles.homeContent}>
+  //<h1 className={styles.homeTitle}>Find your perfect accommodation</h1>
   return (
     <div className={styles.home}>
+      <div className={styles.bgImageContainer}>
+        <img src={backgroundImage} alt="background" />
+        <div className={styles.searchBarWrapper}>
+          <SearchBar/>
+        </div>
+      </div>
       <h1>Home</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home;
+export default Home;   
