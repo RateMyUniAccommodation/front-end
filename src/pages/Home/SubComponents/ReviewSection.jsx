2c961aa5beaff63a3de187e373723435e89388aa
Emile Mathieu <emilepc2003@hotmail.com>
2023-03-10 21:11:02 +0000

    fixed small bug className and added image

--- src/pages/Home/SubComponents/ReviewSection.jsx
+++ src/pages/Home/SubComponents/ReviewSection.jsx
@@ -3,7 +3,7 @@ import styles from "./ReviewSection.module.css";
 import Caroussel from "../../../Components/UI/Caroussel/Caroussel";
 const ReviewSection = () => {
   return (
-    <div class={styles.reviewSection}>
+    <div className={styles.reviewSection}>
       <h1>Our Reviews</h1>
       <Caroussel />
     </div>
--- src/pages/Home/SubComponents/TextImageSection.jsx
+++ src/pages/Home/SubComponents/TextImageSection.jsx
@@ -4,15 +4,15 @@ import thinkIcon from "../../../assets/icons/thinking.png";
 
 const TextImageSection = (props) => {
   return (
-    <div class={styles.container}>
-      <div class={styles.content}>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <h1>Difficulty finding Accommodation Information ?</h1>
         <p>
           RateMyUniAccom let's you get reviews for your university accomodation from other students
           and make a choice on where to stay.
         </p>
       </div>
-      <div class={styles.image}>
+      <div className={styles.image}>
         <img src={thinkIcon} alt="Your Image" />
       </div>
     </div>
