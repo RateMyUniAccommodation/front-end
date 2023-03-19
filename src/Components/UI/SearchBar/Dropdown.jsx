cbdc69289e16ae6b0be3b3757440fc3b897c4640
Emile Mathieu <emilepc2003@hotmail.com>
2023-03-19 01:44:09 +0000

    api fetch changes, will implement more

--- src/Components/UI/SearchBar/Dropdown.jsx
+++ src/Components/UI/SearchBar/Dropdown.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import fetchUniversities from "../../../api/api";
+import { fetchUniversities } from "../../../api/api";
 import { useNavigate } from "react-router-dom";
 import styles from "./Dropdown.module.css";
 
--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,17 @@
-const fetchUniversities = async () => {
+export const fetchUniversities = async () => {
   const response = await fetch("/universities.json");
   const universities = await response.json();
   return universities;
 };
 
-export default fetchUniversities;
+// export const fetchProfiles = async () => {
+//   const response = await fetch("/users.json");
+//   const users = await response.json();
+//   return users;
+// };
+
+export default {
+  fetchUniversities,
+};
+
+
