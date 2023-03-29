7ac218c66706fba67955673edc6b7eb543706e9e
JustAdamCain <03adamcain@gmail.com>
2023-03-29 20:13:15 +0100

    Changed dynamic routes to add coords

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment} from "react";
+import React, { Fragment } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth/Auth";
 import Home from "./pages/Home/Home";
@@ -8,18 +8,33 @@ import Error from "./pages/Error/Error";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import University from "./pages/University/University";
+
+import UniversityData from "./assets/data/universityData.json"
+
+import MapPage from "./pages/University/University"
+
+const data = UniversityData
+
 const App = () => {
+
+  const universityPages = UniversityData.map((page) => {
+    const { id, name, lat, lon } = page;
+    return (
+      <Route key={id} path={`/university/${name}`}
+        element={<MapPage title={name} lat={lat} lon={lon} />
+        } />
+    )
+  })
+
   return (
     <Fragment>
       <Navbar />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
+        {universityPages}
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="university">
-          <Route path=":id" element={<University />} />
-        </Route>
         <Route path="auth" element={<Auth />} />
         <Route path="*" element={<Error />} />
       </Routes>
--- src/pages/University/University.jsx
+++ src/pages/University/University.jsx
@@ -3,12 +3,11 @@ import { useParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import styles from "./University.module.css";
 
-const University = () => {
-  const { id } = useParams();
+const University = (props) => {
+  const{title, lat, lon} = props;
   return (
     <div className={styles.layoutContainer}>
-      {/* <h1>{id} Reviews</h1> */}
-      <MapContainer className={styles.map} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <MapContainer className={styles.map} center={[lat, lon]} zoom={15} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
