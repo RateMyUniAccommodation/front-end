10e435c6383f22f01e94d986fe01800f3a7f9965
JustAdamCain <03adamcain@gmail.com>
2023-04-18 15:00:16 +0100

    added api endpoints

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,5 @@
+import apiService from "./apiService";
+
 export const fetchUniversities = async () => {
   const response = await fetch("/mock-data/universities.json");
   const universities = await response.json();
@@ -10,15 +12,44 @@ export const fetchProfiles = async () => {
    return users;
  };
 
+// Review API
+
+export const getReviews = async (id) => {
+  const response = await apiService.get(`/api/reviews/${id}`);
+  const reviews = await response.json();
+  return reviews;
+}
+
+export const postReview = async (id, comment, rating, accomId, userId) => {
+  const data = {
+    "comment": comment,
+    "rating": rating,
+    "accom_id": accomId,
+    "user_id": userId
+  }
+  const response = await apiService.post(`/api/reviews/${id}`, data)
+  return response.json()["message"];
+}
+
+// Map API
+
+export const getMapData = async (lat,lon) => {
+  const response = await apiService.get(`map/get/${lat}/${lon}`);
+  const mapData = await response.json();
+  return mapData;
+}
+
+// Auth API
+
 export const checkEmailExists = async () => {
-  const response  = await fetch("/mock-data/users.json");
-  const emailExists = true;
+  const response  = await apiService.get("auth/checkUsername");
+  const emailExists = response.json()["status"];
   return emailExists
 }
 
 export const checkUsernameExists = async () => {
-  const response = await fetch("");
-  const usernameExists = true;
+  const response = await fetch("/auth/checkEmail");
+  const usernameExists = response.json()["status"];
   return usernameExists
 }
 
@@ -37,6 +68,9 @@ export const register = async () => {
 export default {
   fetchUniversities,
   fetchProfiles,
+  getMapData,
+  getReviews,
+  postReview,
   checkEmailExists,
   checkUsernameExists,
   login,
