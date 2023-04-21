0aa57a112b4a6244ac247126bb36c132e6111b30
Emile Mathieu <emilepc2003@hotmail.com>
2023-04-21 18:00:52 +0200

    Fixed review pages

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,22 +1,16 @@
 import React, { useEffect, useState } from "react";
-import styles from "./accommodation.module.css"
-import { useLocation } from 'react-router-dom';
+import styles from "./Accommodation.module.css";
+import { useLocation } from "react-router-dom";
 import { getReviews } from "../../api/api";
 import ReviewCard from "./SubComponents/ReviewCard";
-import ReviewForm from "./SubComponents/ReviewForm";
 
-const Accommodation = ({accomInfo}) => {
-  const [reviews, setReviews] = useState([])
+const Accommodation = ({ accomInfo }) => {
+  const [reviews, setReviews] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   const { state } = useLocation();
-  console.log(state);
-  const data = state["accom_info"]["item"]
-
-  const dynamicBackground = {
-    backgroundImage: `url(${data.image_link})`,
-  }
+  const data = state["accom_info"]["item"];
 
   useEffect(() => {
     const fetchReviews = async () => {
@@ -33,42 +27,20 @@ const Accommodation = ({accomInfo}) => {
     fetchReviews();
   }, [data.id]);
 
-  const addReview = (review) => {
-    setReviews([review,...reviews]);
-  };
-
-  const handleReviewCreation = (rating, comment) => {
-    const newReview = {
-      id:  reviews.length + 1,
-      rating: rating,
-      comment: comment,
-      username: "You",
-      create_time: new Date().toISOString()
-    }
-    console.log(newReview)
-    addReview(newReview);
-  }
-
-  return <>
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
-        <div className={styles.headerImage} style={dynamicBackground} />
-        <div className={styles.headerContent}>
-          <h1 className={styles.accomName}>{data.name}</h1>
-        </div>
-      </div>
-
-
-      <div className={styles.reviewContainer}>
-        <ReviewForm onSubmit={handleReviewCreation} accomId={data.id} />
-        <h2>Reviews</h2>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error</p>}
-        {!loading && !error && reviews.map((review) => (
-          <ReviewCard key={review.id} review={review} />
-        ))}
+  return (
+    <div className={styles.accomGrid}>
+      <h1>{data.name}</h1>
+      <h2>Reviews</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error</p>}
+      <div className={styles.reviewGrid}>
+        {!loading &&
+          !error &&
+          reviews.map((review) => (
+            <ReviewCard key={review.id} review={review} />
+          ))}
       </div>
     </div>
-  </>;
-}
+  );
+};
 export default Accommodation;
--- src/pages/Accommodation/SubComponents/StarRating.jsx
+++ src/pages/Accommodation/SubComponents/StarRating.jsx
@@ -1,17 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { StarIcon } from "@heroicons/react/24/solid";
 import styles from "./StarRating.module.css";
 
 const StarRating = ({ rating }) => {
   const numStars = 5;
-  const fullStars = rating;
 
   const stars = [];
   for (let i = 0; i < numStars; i++) {
-    stars.push(i < fullStars ? <StarIcon key={i} className={styles.solidStar} /> : <StarIcon key={i} className={styles.emptyStar} />);
+    stars.push(i < rating ? <StarIcon key={i} className={styles.solidStar} /> : <StarIcon key={i} className={styles.emptyStar} />);
   }
   
-  return <div style={{ display: "flex" }}>{stars}</div>;
+  return <div className={styles.starsWrapper}>{stars}</div>
 };
 
 export default StarRating;
