b16f47673819356e18ec46bbd8f2d8524eae5287
Emile Mathieu <emilepc2003@hotmail.com>
2023-04-21 18:33:01 +0200

    Revert "console logging"
    This reverts commit d61d3336a5d88cb9d277f654c672162a8dae3c57.

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -27,8 +27,6 @@ const Accommodation = ({ accomInfo }) => {
     fetchReviews();
   }, [data.id]);
 
-  console.log(data);
-
   return (
     <div className={styles.accomGrid}>
       <h1>{data.name}</h1>
